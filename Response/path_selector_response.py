1f01e73277f40cbbd1bf182524cb432fda62b71c
MrXnneHang <XnneHang@gmail.com>
2024-07-20 17:06:05 +0800

    响应会在这里

---
+++ Response/path_selector_response.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QMainWindow,QFileDialog
+
+from UI.path_selector import Ui_PathSelector
+
+
+class RSP_PathSelector(QMainWindow,Ui_PathSelector):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setupUi(self)
+        self.path = ""
+        self.path_selector_1.clicked.connect(lambda:self.open_file_dialog(self.path_line_1))
+        self.path_selector_2.clicked.connect(lambda:self.open_file_dialog(self.path_line_2))
+        self.path_selector_3.clicked.connect(lambda:self.open_file_dialog(self.path_line_3))
+        self.path_selector_4.clicked.connect(lambda:self.open_file_dialog(self.path_line_4))
+        
+
+    def open_file_dialog(self,line_edit):
+        # 弹出文件对话框
+        path = QFileDialog.getOpenFileName(self, "选择文件位置", "")
+        if path:
+            # path-like:('D:/program/Designer/README.md', 'All Files (*)')
+            line_edit.setText(path[0])  # 将选择的路径设置到LineEdit中
+
+            # print(f"{self.path_line_1.text()}\n"
+            #       f"{self.path_line_2.text()}\n"
+            #       f"{self.path_line_3.text()}\n"
+            #       f"{self.path_line_4.text()}\n")
+
+
