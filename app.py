791fda6da0ad2a5758dc86e32eddb5bf31da7a1d
MrXnneHang <XnneHang@gmail.com>
2024-07-20 19:22:13 +0800

    完成选中返回True

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ import sys
 from PyQt5 import QtWidgets
 
 from Response.path_selector_response import RSP_PathSelector
+from Response.chart_selector_response import RSP_ChartSelector
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    main_window = RSP_PathSelector()
-    main_window.hidden_Above()
+
+    # # Path Selector
+    # main_window = RSP_PathSelector()
+    # main_window.hidden_Above()
+    # main_window.show()
+
+    # Chart Seletor
+    main_window = RSP_ChartSelector()
     main_window.show()
+
+    
     sys.exit(app.exec_())
