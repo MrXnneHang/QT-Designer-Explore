64069e9cf2c1994a62f7991fb92147f2767b7450
MrXnneHang <XnneHang@gmail.com>
2024-07-21 11:28:20 +0800

    ClickQLabel，完成click监控

--- modules/Click_QLabel.py
+++ modules/Click_QLabel.py
@@ -2,11 +2,14 @@ from PyQt5.QtWidgets import QLabel
 from PyQt5.QtCore import pyqtSignal, Qt
 
 class ClickQLabel(QLabel):
-    clicked = pyqtSignal()
+    """可以被触发的Label,制作动态效果.
 
+    """
+    clicked = pyqtSignal() #.clicked.connect()
     def __init__(self, parent=None):
         super().__init__(parent)
-
+        self.background_img = "" # path
+        
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.clicked.emit()
