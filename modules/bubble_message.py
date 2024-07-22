4591f16be296b3f7e173c1d1fd88735f0f9c1869
MrXnneHang <XnneHang@gmail.com>
2024-07-22 11:51:38 +0800

    将bubble_message类放到一起

--- modules/bubble_message.py
+++ modules/bubble_message.py
@@ -275,3 +275,21 @@ class ChatWidget(QWidget):
         self.scrollArea.update()
         # self.scrollArea.repaint()
         # self.verticalScrollBar().setMaximum(self.scrollAreaWidgetContents.height())
+
+
+from datetime import datetime
+
+class Message_Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.w1 = ChatWidget()
+        # 获取当前日期和时间
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+        # 创建 Notice 对象
+        time_message = Notice(current_time)
+        self.w1.add_message_item(time_message)
+        w2 = QLabel("你需要文件1文件2")
+        layout.addWidget(self.w1)
+        layout.addWidget(w2)
+        self.setLayout(layout)
