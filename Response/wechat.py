96b04884809a52b57a40fae13b8da8cc1dd9b463
MrXnneHang <XnneHang@gmail.com>
2024-07-23 10:17:13 +0800

    使用弹性界面

--- Response/wechat.py
+++ Response/wechat.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QSize
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout
 
 
@@ -7,13 +8,17 @@ from modules.bubble_message import MessageType, BubbleMessage,Message_Window
 
 from UI.wechat_main import Ui_Chat_Main
 from tools.yml import read_history
+from tools.window_auto_scale import calculate_screen_scaling_ratio
 class RSP_WeChat(QDialog, Ui_Chat_Main):
     """对话窗口
     @func:
     """
+
     def __init__(self):
         super(RSP_WeChat, self).__init__()
         self.setupUi(self)
+        self.ratio = calculate_screen_scaling_ratio()
+        self.resize_window()
 
 
         # 创建 RSP_Wechat 实例
@@ -27,6 +32,16 @@ class RSP_WeChat(QDialog, Ui_Chat_Main):
 
     def initialize(self):
         self.add_history()
+    
+    def resize_window(self):
+        # 获取当前窗口大小
+        current_size = self.size()
+        # 根据比例调整窗口大小
+        new_width = int(current_size.width() * self.ratio)
+        new_height = int(current_size.height() * self.ratio)
+        # 调整窗口大小
+        self.resize(QSize(new_width, new_height))
+
 
     def add_history(self):
         chats = read_history()
