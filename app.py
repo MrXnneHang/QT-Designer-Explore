0922b059d9b2288cc045fd72ebc6b6d7135722d2
MrXnneHang <XnneHang@gmail.com>
2024-07-23 13:25:48 +0800

    完美解决highdpi问题

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
-
+from time import sleep
 from Response.path_selector_response import RSP_PathSelector
 from Response.chart_selector_response import RSP_ChartSelector
 from Response.tab_response import RSP_Tab
@@ -9,9 +9,11 @@ from Response.click_label_response import RSP_ClickLabel
 from Response.wechat import RSP_WeChat
 
 if __name__ == "__main__":
+    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QtWidgets.QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     app = QtWidgets.QApplication(sys.argv)
-    #app.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # 自适应高分屏
-    #app.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    app.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # 自适应高分屏
+    app.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
     # # Path Selector
     # main_window = RSP_PathSelector()
